af95947ff76e6438a859ed3d3df3973dd418fd27
Yana Pavlyk <pavlikyg@gmail.com>
2022-10-01 23:57:12 +0300

    added local storage logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 
+const CONTACTS_KEY = 'contacts';
+
 export const App = () => {
-  const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState(isSavedContacts());
   const [filter, setFilter] = useState('');
+
+  function isSavedContacts() {
+    let savedContacts = [];
+    const localStorageContacts = localStorage.getItem(CONTACTS_KEY);
+
+    if (localStorageContacts?.length > 0) {
+      return (savedContacts = JSON.parse(localStorageContacts));
+    }
+    return savedContacts;
+  }
+
+  useEffect(() => {
+    return localStorage.setItem(CONTACTS_KEY, JSON.stringify(contacts));
+  }, [contacts]);
 
   const isDuplicate = ({ name }) => {
     const result = contacts.find(
